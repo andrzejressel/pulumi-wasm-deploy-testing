d3a5f1e6fbed078de23efbda0d3c6b00b93cb6d2
Andrzej Ressel <github@andrzejressel.pl>
2024-03-04 03:14:37 +0100

    Cleanup

--- pulumi_rust/src/pulumi.rs
+++ pulumi_rust/src/pulumi.rs
@@ -43,9 +43,7 @@ struct MyState {
 impl server::component::pulumi_wasm::external_world::Host for MyState {
     async fn register_resource(&mut self, request: Vec<u8>) -> wasmtime::Result<Vec<u8>> {
         let _result = self.register_async(request).await?;
-        // executor::block_on(result)?;
         Ok(vec![])
-        // run_blocking(self.register_async(request))
     }
 }
 
@@ -115,8 +113,6 @@ impl Pulumi {
 
         wasmtime_wasi::preview2::command::add_to_linker(&mut linker).unwrap();
 
-        // Plugin::add_to_linker(&mut linker, |state: &mut SimplePluginCtx| &mut state.logger).unwrap();
-
         let table = ResourceTable::new();
 
         let wasi_ctx = WasiCtxBuilder::new()
@@ -161,97 +157,5 @@ impl Pulumi {
             .await?;
 
         Ok(())
-
-        // self.plugin
-        //     .component_pulumi_wasm_pulumi_interface()
     }
-
-    // pub async fn test(&self) -> Result<(), Error> {
-    //     let object = prost_types::Struct {
-    //         fields: BTreeMap::from([
-    //             (
-    //                 "length".to_string(),
-    //                 prost_types::Value {
-    //                     kind: Option::from(prost_types::value::Kind::NumberValue(10.into())),
-    //                 },
-    //             ),
-    //             (
-    //                 "minNumeric".to_string(),
-    //                 prost_types::Value {
-    //                     kind: Option::from(prost_types::value::Kind::NumberValue(5.into())),
-    //                 },
-    //             ),
-    //         ]),
-    //     };
-    //
-    //     let request = RegisterResourceRequest {
-    //         r#type: "random:index/randomString:RandomString".to_string(),
-    //         name: "test_string_123".to_string(),
-    //         parent: "".to_string(),
-    //         custom: true,
-    //         object: Some(object),
-    //         protect: false,
-    //         dependencies: vec![],
-    //         provider: "".to_string(),
-    //         property_dependencies: Default::default(),
-    //         delete_before_replace: false,
-    //         version: "".to_string(),
-    //         ignore_changes: vec![],
-    //         accept_secrets: true,
-    //         additional_secret_outputs: vec![],
-    //         alias_ur_ns: vec![],
-    //         import_id: "".to_string(),
-    //         custom_timeouts: None,
-    //         // custom_timeouts: Some(CustomTimeouts {
-    //         //     create: "5m".to_string(),
-    //         //     update: "5m".to_string(),
-    //         //     delete: "5m".to_string(),
-    //         // }),
-    //         delete_before_replace_defined: false,
-    //         supports_partial_values: false,
-    //         remote: false,
-    //         accept_resources: false,
-    //         providers: Default::default(),
-    //         replace_on_changes: vec![],
-    //         plugin_download_url: "".to_string(),
-    //         plugin_checksums: Default::default(),
-    //         retain_on_delete: false,
-    //         aliases: vec![],
-    //         deleted_with: "".to_string(),
-    //         alias_specs: false,
-    //         source_position: None,
-    //     };
-    //
-    //     let request_vec = request.encode_to_vec();
-    //
-    //     self.register_resource(request_vec).await?;
-    //
-    //     Ok(())
-    // }
-    //
-    // pub async fn register_resource(&self, content: Vec<u8>) -> Result<(), Error> {
-    //     use prost::Message;
-    //     let engine_url = self
-    //         .pulumi_monitor_url
-    //         .clone()
-    //         .ok_or(Error::msg("engine_url not set"))?;
-    //
-    //     let request = RegisterResourceRequest::decode(&mut content.clone().as_slice())?;
-    //
-    //     let mut client = ResourceMonitorClient::connect(format!("tcp://{engine_url}")).await?;
-    //
-    //     let result = client.register_resource(request).await?;
-    //     use std::io::prelude::*;
-    //
-    //     let mut file = OpenOptions::new()
-    //         .create(true)
-    //         .write(true)
-    //         .append(true)
-    //         .open("my-file")
-    //         .unwrap();
-    //
-    //     writeln!(file, "{:?}", result).unwrap();
-    //
-    //     Ok(())
-    // }
 }
